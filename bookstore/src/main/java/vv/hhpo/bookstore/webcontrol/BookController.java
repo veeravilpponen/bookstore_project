443de57c6b7317fd1ce4fac44a534e9343199958
Veera Vilpponen <veera.vilpponen@myy.haaga-helia.fi>
2018-09-14 16:54:38 +0300

    listpage lisätty

--- bookstore/src/main/java/vv/hhpo/bookstore/webcontrol/BookController.java
+++ bookstore/src/main/java/vv/hhpo/bookstore/webcontrol/BookController.java
@@ -1,16 +1,24 @@
 package vv.hhpo.bookstore.webcontrol;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import vv.hhpo.bookstore.model.BookRepository;
 
 @Controller
-@ResponseBody
 public class BookController {
 	
-	@GetMapping("/addbook")
-	public String addBook() {
-		return "Book not added";
+	//lisätään luokkaan repository attribuuttina
+	//attribuutilta voi pyytää tietokantakäsittelyjä, esim delete, save
+	@Autowired
+	private BookRepository bookRepository;
+	
+	//haetaan kirjojen tiedot tietokannasta
+	@GetMapping("/allbooks")
+	public String listBooks(Model model) {
+		model.addAttribute("books", bookRepository.findAll());
+		return "booklist"; //html
 	}
 
 }
